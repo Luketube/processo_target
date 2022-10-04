4c52075b518f0b4a2baca5eba032b87755513a59
Lucas Farias <lucasfarias48@hotmail.com>
2022-10-04 20:51:20 -0300

    Adicionado formulario e cabeçalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule
