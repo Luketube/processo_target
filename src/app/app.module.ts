bd62c46352d93a1286450ab7e6bd9c65325af9fc
Lucas Farias <lucasfarias48@hotmail.com>
2022-10-20 20:23:22 -0300

    Adicionado inputs com validação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { MatButtonModule} from '@angular/material/button';
 import { MatSortModule } from '@angular/material/sort';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -33,7 +34,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatSortModule,
     MatFormFieldModule,
     MatInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Dados } from '../model/dados';
 import { TransferenciaService } from '../service/transferencia.service';
 
@@ -11,6 +12,10 @@ import { TransferenciaService } from '../service/transferencia.service';
 export class FormularioComponent implements OnInit {
   nome: string = '';
   telefone: any;
+  regex: RegExp = (/^(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/);
+
+  nomeFormControl = new FormControl('', [Validators.required]);
+  telefoneFormControl = new FormControl('', [Validators.pattern(this.regex), Validators.required]);
 
   constructor(private transferencia: TransferenciaService) {}
 
